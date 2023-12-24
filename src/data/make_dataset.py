5c08a7b4730b2f3dc2130aa631c8a05842c9bf78
Mohit Kumar <mkr9395@gmail.com>
2023-12-24 15:06:07 +0530

    params and make dataset added

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,44 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
+import pathlib
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import yaml
+import sys
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+# loading the dataset
+def load_data(data_path):
+    df = pd.read_csv(data_path)
+    return df
 
+# splitting into train and test
+def split_data(df,test_split,seed):
+    train,test = train_test_split(df,test_size=test_split,random_state=seed)
+    return train, test
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
+# save the train and test splits
+def save_data(train,test,output_path):
+    pathlib.Path(output_path).mkdir(parents=True,exist_ok=True) 
+    train.to_csv(output_path + '/train.csv', index=False)
+    test.to_csv(output_path + '/test.csv', index=False)
+    
+    
+def main():
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    params_file = (home_dir / 'params.yaml').as_posix()
+    params = yaml.safe_load(open(params_file))["make_dataset"]
+    
+    #input_file = pathlib.Path(home_dir / 'data/raw/credit_card.csv').as_posix()
+    #print(input_file)
+    
+    data_path = pathlib.Path(home_dir / 'data/raw/creditcard.csv').as_posix()
+    output_path = (home_dir / 'data/processed').as_posix()
+    
+    data = load_data(data_path)
+    train_data, test_data = split_data(data,params['test_split'],params['seed'])
+    save_data(train_data, test_data, output_path)
+    
+if __name__ == "__main__":
     main()
